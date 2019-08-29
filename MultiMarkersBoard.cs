ce15410243af46842ec0ae85d94edf7f4e8cbecd
vjpal3 <vrishalipal@yahoo.com>
2019-08-29 12:50:28 -0400

    Initialize the board for Multi-Marker & Dice Game V2.0

---
+++ MultiMarkersBoard.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinishLine
+{
+    class MultiMarkersBoard
+    {
+        private Player player1;
+        private Player player2;
+        private readonly Die blackDie;
+        private readonly Die redDie;
+        private readonly Deck deck;
+        public readonly List<int> restrictedCards = new List<int> { 0, 1, 2, 11, 12, 13 };
+
+        public MultiMarkersBoard()
+        {
+            player1 = new Player();
+            player2 = new Player();
+            blackDie = new Die(6);
+            redDie = new Die(6);
+            deck = new Deck();
+        }
+
+        public void ValidateDeck(Random rand)
+        {
+            // A valid deck should not have J, Q, K, A , 2 & Joker as its first 3 and last 3 cards.
+            while (restrictedCards.Contains(deck.Cards[0].Value) || restrictedCards.Contains(deck.Cards[1].Value) || restrictedCards.Contains(deck.Cards[2].Value) || restrictedCards.Contains(deck.Cards[deck.Cards.Count - 1].Value) || restrictedCards.Contains(deck.Cards[deck.Cards.Count - 2].Value) || restrictedCards.Contains(deck.Cards[deck.Cards.Count - 3].Value))
+            {
+                RandomExtensions.Shuffle(rand, deck.Cards);
+            }
+        }
+
+        public string StartGame(Random rand)
+        {
+            deck.Shuffle(rand);
+            this.ValidateDeck(rand);
+            deck.ShowDeck();
+
+            return "";
+
+        }
+    }
+}
